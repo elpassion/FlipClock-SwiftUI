3513bfa4f031410940c39930161970d722b0548a
Maciej Gomółka <mgomolka92@gmail.com>
2019-10-14 20:17:47 +0200

    Initial Commit

---
+++ FlipCountdown/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FlipCountdown
+//
+//  Created by Maciej Gomółka on 14/10/2019.
+//  Copyright © 2019 Maciej Gomółka. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
